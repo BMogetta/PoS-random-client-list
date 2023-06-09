12e06e77dbe0872001e1bfde07ef294c49d3e847
BMogetta <brunomogetta@gmail.com>
2023-06-09 18:40:25 -0300

    feat: initial commit

---
+++ generate_client_list.py
@@ -0,0 +1,51 @@
+import random
+import json
+import uuid
+import sys
+from num_random import generate_number
+
+with open("currencies_list.json") as jsonfile:
+    currencies_list = json.load(jsonfile)
+
+
+def generate_client_list(length):
+    client_list = []
+
+    for i in range(length):
+        _uuid = uuid.uuid4()
+        client_id = str(_uuid)  # User IDs start from '1'
+        num_tokens_accounts = random.randint(1, 10)  # Generate a random number between 1 and 10
+
+        accounts = []
+
+        for j in range(num_tokens_accounts):
+            random_token = random.randint(0, len(currencies_list)-1)
+            token_list = currencies_list[random_token]
+            token_name = token_list[0]
+            account_tokens = generate_number(token_list[1], token_list[2])  # Generate a random number between 1e-9 and 1e9
+
+            accounts.append((token_name, account_tokens))
+
+        client_data = {
+            client_id: accounts,
+        }
+
+        client_list.append(client_data)
+
+    return client_list
+
+def run ():
+    # Check if an argument is provided
+    if len(sys.argv) > 1:
+        # Get the value from the command-line argument
+        arg = sys.argv[1]
+
+        # Call the function with the argument
+        client_list = generate_client_list(int(arg))
+    else:
+        client_list = generate_client_list(10000)
+
+    with open("client_list.json", "w") as jsonfile:
+        json.dump(client_list, jsonfile)
+    return client_list
+run()
---
+++ num_random.py
@@ -0,0 +1,46 @@
+import random
+import math
+
+def get_decimal_length(number):
+    string = "{:.20f}".format(number).rstrip('0')
+    if '.' in string:
+        decimals = string.split('.')[1].rstrip('0')
+        decimal_length = len(decimals)
+        return decimal_length
+    else:
+        return 0
+
+def generate_number(minimum, maximum, displacement=3):
+    divisions = int(math.log10(maximum / minimum))
+    
+    scale_factor = math.log10(maximum / minimum) / divisions
+    decimals = get_decimal_length(minimum)
+
+    ranges = []
+    probabilities = []
+    total_sum = 0
+    for i in range(divisions):
+        lower_limit = minimum * 10**(i * scale_factor)
+        upper_limit = minimum * 10**((i + 1) * scale_factor)
+        range_ = (lower_limit, upper_limit)
+        ranges.append(range_)
+        if i < divisions // displacement:
+            probability = (i + 1) / (divisions // displacement + 1)
+        else:
+            probability = (divisions - i) / ((displacement-1) * divisions // displacement)
+        probabilities.append(probability)
+        total_sum += probability
+    
+    probabilities = [p / total_sum for p in probabilities]
+    random_number = round(random.random(), decimals)
+
+    cumulative_probability = 0
+    for range_, probability in zip(ranges, probabilities):
+        cumulative_probability += probability
+        if random_number < cumulative_probability:
+            adjusted_number = round(random.uniform(range_[0], range_[1]), decimals)
+            return str(int(adjusted_number*(10**decimals)))
+
+    adjusted_number = round(random.uniform(ranges[-1][0], ranges[-1][1]), decimals)
+
+    return str(int(adjusted_number*(10**decimals)))
